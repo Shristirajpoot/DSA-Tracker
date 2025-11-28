6b0e31605827cb3be1bc5253c76daa574d54e0cc
👩‍💻Shristi Rajpoot <147296860+Shristirajpoot@users.noreply.github.com>
2025-11-28 18:48:48 +0530

    Create max_product_subarray.cpp

---
+++ max_product_subarray.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int maxProduct(vector<int>& nums) {
+    int currMax = nums[0], currMin = nums[0], ans = nums[0];
+    
+    for(int i = 1; i < nums.size(); i++) {
+        if(nums[i] < 0) swap(currMax, currMin);
+        currMax = max(nums[i], currMax * nums[i]);
+        currMin = min(nums[i], currMin * nums[i]);
+        ans = max(ans, currMax);
+    }
+    return ans;
+}
+
+int main() {
+    vector<int> nums = {2,3,-2,4};
+    cout << maxProduct(nums);
+}
