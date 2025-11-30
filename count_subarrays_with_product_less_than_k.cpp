8a7c8fdcccb5336d50e1a69bdf0fc58f06740954
👩‍💻Shristi Rajpoot <147296860+Shristirajpoot@users.noreply.github.com>
2025-11-30 07:55:17 +0530

    Create count_subarrays_with_product_less_than_k.cpp

---
+++ count_subarrays_with_product_less_than_k.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int numSubarrayProductLessThanK(vector<int>& nums, int k) {
+        if (k <= 1) return 0;
+
+        long long prod = 1;
+        int left = 0, count = 0;
+
+        for(int right = 0; right < nums.size(); right++){
+            prod *= nums[right];
+
+            while(prod >= k) {
+                prod /= nums[left];
+                left++;
+            }
+            count += right - left + 1;
+        }
+        return count;
+    }
+};
