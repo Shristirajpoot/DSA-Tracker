87079d75ef7ce4694832bb34b7fc653213f9e4a9
👩‍💻Shristi Rajpoot <147296860+Shristirajpoot@users.noreply.github.com>
2026-01-03 23:32:39 +0530

    Add bipartite graph check implementation
    Implement a function to check if a graph is bipartite using BFS.

---
+++ bipartite_graph_check.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+    vector<vector<int>> g(n);
+    for (int i = 0; i < m; i++) {
+        int u, v;
+        cin >> u >> v;
+        g[u].push_back(v);
+        g[v].push_back(u);
+    }
+
+    vector<int> color(n, -1);
+    for (int i = 0; i < n; i++) {
+        if (color[i] == -1) {
+            queue<int> q;
+            q.push(i);
+            color[i] = 0;
+            while (!q.empty()) {
+                int u = q.front(); q.pop();
+                for (int v : g[u]) {
+                    if (color[v] == -1) {
+                        color[v] = color[u] ^ 1;
+                        q.push(v);
+                    } else if (color[v] == color[u]) {
+                        cout << "NO";
+                        return 0;
+                    }
+                }
+            }
+        }
+    }
+    cout << "YES";
+}
