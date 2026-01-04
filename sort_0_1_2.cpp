160af4329f101b129895afda117e0d2374b9fd1b
👩‍💻Shristi Rajpoot <147296860+Shristirajpoot@users.noreply.github.com>
2026-01-04 15:03:23 +0530

    Implement Dutch National Flag algorithm for sorting

---
+++ sort_0_1_2.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void sort012(vector<int>& a) {
+    int low = 0, mid = 0, high = a.size() - 1;
+
+    while (mid <= high) {
+        if (a[mid] == 0) swap(a[low++], a[mid++]);
+        else if (a[mid] == 1) mid++;
+        else swap(a[mid], a[high--]);
+    }
+}
+
+int main() {
+    vector<int> a = {0,2,1,2,0,1};
+    sort012(a);
+    for (int x : a) cout << x << " ";
+}
